709440241965cdcbd96d467aaf2b5e7644813322
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-05-16 20:11:25 -0300

    first commit

---
+++ src/main/java/one/digitalinnovation/presonapi/controller/PersonController.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.presonapi.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/people")
+public class PersonController {
+
+    @GetMapping
+    public String getBook(){
+        return "API Test!";
+    }
+
+}
---
+++ src/test/java/one/digitalinnovation/presonapi/PresonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.presonapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
