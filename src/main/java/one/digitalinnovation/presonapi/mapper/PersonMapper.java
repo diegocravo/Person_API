ab3d2f25380ce8007aafa4b6ff39af1c8de1e2b9
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-05-17 03:31:10 -0300

    update

--- src/main/java/one/digitalinnovation/presonapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/presonapi/mapper/PersonMapper.java
@@ -12,7 +12,7 @@ public interface PersonMapper {
 
 	PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 	
-	@Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyy")
+	@Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
 	Person toModel(PersonDTO personDTO);
 	
 	PersonDTO toDTO(Person person);
