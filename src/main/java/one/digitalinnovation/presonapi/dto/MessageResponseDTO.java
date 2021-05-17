18eab885891321ba3af97ce1baf221629db7377e
Diego Cravo Teixeira <cravo.diego@gmail.com>
2021-05-17 03:56:19 -0300

    fixes

--- src/main/java/one/digitalinnovation/presonapi/dto/MessageResponseDTO.java
+++ src/main/java/one/digitalinnovation/presonapi/dto/MessageResponseDTO.java
@@ -3,12 +3,9 @@ package one.digitalinnovation.presonapi.dto;
 import lombok.Builder;
 import lombok.Data;
 
-import java.security.cert.CertPathBuilder;
-
 @Data
 @Builder
 public class MessageResponseDTO {
 
     private String message;
-
 }
